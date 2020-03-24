367191d87412a09f902db4c6ca22e33cd2279aa7
Matt Ewell <matt.ewell@gmail.com>
2020-03-24 09:36:56 -0600

    Add mocha using beverage as mixin

--- decorator/lib/beverage.rb
+++ decorator/lib/beverage.rb
@@ -1,7 +1,15 @@
-class Beverage
-  def initialize
-    # Honestly not sure if I would ever do this in ruby...
-    raise StandardError, "This is an abstract class?"
+module Beverage
+  def initialize(beverage: nil)
+    @beverage = beverage
+  end
+
+  def cost
+    cost = base_cost + @beverage.cost if @beverage
+    cost || base_cost
+  end
+
+  def base_description
+    self.class.to_s.downcase
   end
 
   def description
---
+++ decorator/spec/mocha_spec.rb
@@ -0,0 +1,6 @@
+require './spec/beverage_examples'
+require './lib/mocha'
+
+describe Mocha do
+  it_behaves_like "a beverage"
+end
