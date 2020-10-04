b8d4e859ffa58de94f6152b6a5e50d3d4d5033db
Matt Ewell <matt.ewell@gmail.com>
2020-10-04 14:01:43 -0600

    Add register_observer to flock

--- compound_patterns/ducks/lib/flock.rb
+++ compound_patterns/ducks/lib/flock.rb
@@ -7,6 +7,10 @@ class Flock
     @ducks << duck
   end
 
+  def register_observer(observer)
+    @ducks.each { |duck| duck.register_observer(observer) }
+  end
+
   def quack
     @ducks.each(&:quack)
   end
--- compound_patterns/ducks/spec/flock_spec.rb
+++ compound_patterns/ducks/spec/flock_spec.rb
@@ -1,5 +1,6 @@
 require './lib/flock'
 require './lib/mallard_duck'
+require './spec/duck_examples'
 
 RSpec.describe Flock do
   it_behaves_like "a duck", Flock.new
@@ -27,5 +28,24 @@ RSpec.describe Flock do
 
     expect(duck1).to have_received(:quack)
     expect(duck2).to have_received(:quack)
+  end
+
+  it "registers observers with the ducks" do
+    duck1 = double(:duck)
+    duck2 = double(:duck)
+    allow(duck1).to receive(:register_observer)
+    allow(duck2).to receive(:register_observer)
+    observer = double(:observer)
+
+    flock = Flock.new
+    flock.add(duck1)
+    flock.add(duck2)
+
+    flock.register_observer(observer)
+
+    expect(duck1).to have_received(:register_observer).exactly(1).times
+                       .with(observer)
+    expect(duck2).to have_received(:register_observer).exactly(1).times
+                       .with(observer)
   end
 end
