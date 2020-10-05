50dff2ea018670f88e2646769e1c088d6b7231f9
Matt Ewell <matt.ewell@gmail.com>
2020-10-05 07:57:48 -0600

    Reorder duck simulator

--- compound_patterns/ducks/duck_simulator.rb
+++ compound_patterns/ducks/duck_simulator.rb
@@ -9,22 +9,25 @@ class DuckSimulator
   end
 
   def perform
-    zoologist = Zoologist.new
-
-    mallard_ducks = Flock.new
-    5.times { mallard_ducks.add(@duck_factory.create_mallard_duck) }
-
     all_ducks = Flock.new
 
+    # some assorted ducks
     ducks = ["mallard_duck", "decoy_duck", "darkwing_duck", "rubber_duck"]
     ducks.each do |type|
       duck = @duck_factory.send("create_#{type}".to_sym)
       all_ducks.add(duck)
     end
 
+    # our goose
     all_ducks.add(GooseAdapter.new(Goose.new))
+
+    # mallards only, nested flock
+    mallard_ducks = Flock.new
+    5.times { mallard_ducks.add(@duck_factory.create_mallard_duck) }
     all_ducks.add(mallard_ducks)
 
+    # our observer
+    zoologist = Zoologist.new
     all_ducks.register_observer(zoologist)
 
     all_ducks.quack
